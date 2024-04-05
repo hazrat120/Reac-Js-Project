076cd3d08f269d6ef5e3e93aaf3ba432a7c22214
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2024-04-05 23:41:53 +0600

    problem solve

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 //firebase configuration
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STRAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_STRAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_APP_ID,
 });
 
 export default app;
